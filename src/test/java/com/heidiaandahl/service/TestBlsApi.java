8daf2fab62886d76cb107cd88fb65e9a47db4f05
Heidi Aandahl <hlaandahl@gmail.com>
2019-04-11 21:04:55 -0500

    Adds passing test demonstrating how to get Web Dev median wage from api call

--- src/test/java/com/heidiaandahl/service/TestBlsApi.java
+++ src/test/java/com/heidiaandahl/service/TestBlsApi.java
@@ -1,9 +1,12 @@
 package com.heidiaandahl.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,6 +18,7 @@ public class TestBlsApi {
         // TODO - obtain api key from properties and apply it here to allow more data calls
         // ?registrationkey=
 
+        // api request for web developer wage
         WebTarget target =
                 client.target("https://api.bls.gov/publicAPI/v2/timeseries/data/" +
                         "OEUN000000000000015113413");
@@ -26,6 +30,37 @@ public class TestBlsApi {
         String response = target.request(MediaType.APPLICATION_JSON).get(String.class);
         // Note: this doesn't work because the respones includes responseTime, which varies
                 // assertEquals(expectedResponse, response);
-     }
+
+        // I think how this might work is we want to get the DataItem for which "latest" is "true" and return its "value"
+        // I'm not sure how this might look if a career has data for more than one year
+        // For the test case, the only value is 69430 so I'll try to get at that
+
+        ObjectMapper mapper = new ObjectMapper();
+        Response webDevResponse = mapper.readValue(response, Response.class);
+
+        // For teh results get the first series, tehn get the data, in particular value?
+        List<DataItem> webDevDataItems = webDevResponse.getResults().getSeries().get(0).getData();
+
+        // List<Integer> webDevWages;
+        int wagesSum = 0;
+        int numberOfWages = 0;
+
+        for (DataItem item : webDevDataItems) {
+            if (item.getLatest().equals("true")) {
+                // webDevWages.add(item.getValue().valueOf());
+                wagesSum += Integer.parseInt(item.getValue());
+                numberOfWages += 1;
+            }
+        }
+
+        int averageRecentWage = 0;
+
+        if (numberOfWages > 0) {
+            averageRecentWage = Math.round(wagesSum / numberOfWages);
+        }
+
+        assertEquals(69430, averageRecentWage);
+
+    }
 }
 
